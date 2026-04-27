1b75994fce45e326af6dbaa5d3ce26463cf72de4
nabilasya77 <nabilacheisya_7@student.uns.ac.id>
2026-04-27 19:55:26 +0700

    up

--- api/home.php
+++ api/home.php
@@ -1,6 +1,11 @@
 <?php
-session_start(); 
-require 'koneksi.php'; 
+session_start();
+
+var_dump($_SESSION);
+exit();
+
+require 'koneksi.php';
+?>
 
 // Ambil data destinasi dari DATABASE
 $query_wisata = mysqli_query($koneksi, "SELECT * FROM destinasi ORDER BY id DESC");
